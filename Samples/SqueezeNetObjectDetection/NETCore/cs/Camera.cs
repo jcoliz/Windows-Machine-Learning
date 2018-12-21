816bb4431d2020525d70162e2e99a3cf6a6e6f49
James Coliz <jcoliz@mail.com>
2018-12-20 16:56:09 -0800

    Camera enumation returns all groups and devices

--- Samples/SqueezeNetObjectDetection/NETCore/cs/Camera.cs
+++ Samples/SqueezeNetObjectDetection/NETCore/cs/Camera.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Graphics.Imaging;
@@ -12,38 +13,35 @@ namespace SampleModule
 {
     static class Camera
     {
-        public static async Task<Tuple<MediaFrameSourceGroup, MediaFrameSourceInfo>> EnumFrameSourcesAsync()
+        public static async Task<Dictionary<MediaFrameSourceGroup, List<MediaFrameSourceInfo>>> EnumFrameSourcesAsync()
         {
+            var result = new Dictionary<MediaFrameSourceGroup, List<MediaFrameSourceInfo>>();
             MediaFrameSourceInfo result_info = null;
             MediaFrameSourceGroup result_group = null;
             var sourcegroups = await AsAsync(MediaFrameSourceGroup.FindAllAsync());
             Log.WriteLine("found {0} Source Groups", sourcegroups.Count);
             foreach (var g in sourcegroups)
             {
+                result[g] = new List<MediaFrameSourceInfo>();
+
                 var sourceinfos = g.SourceInfos;
                 Log.WriteLine("Source Group {0}", g.Id);
                 Log.WriteLine("             {0}", g.DisplayName);
                 Log.WriteLine("             with {0} Sources:", sourceinfos.Count);
                 foreach (var s in sourceinfos)
                 {
+                    result[g].Add(s);
+
                     var d = s.DeviceInformation;
                     Log.WriteLine("\t{0}", s.Id);
                     Log.WriteLine("\t\tKind {0}", s.SourceKind);
                     Log.WriteLine("\t\tDevice {0}", d.Id);
                     Log.WriteLine("\t\t       {0}", d.Name);
                     Log.WriteLine("\t\t       Kind {0}", d.Kind);
-                    if (result_info == null)
-                    {
-                        result_info = s; // for now just pick the first thing we find
-                    }
                 }
                 Log.WriteLine("\r\n");
-                if (result_group == null)
-                {
-                    result_group = g; // for now just pick the first thing we find
-                }
             }
-            return new Tuple<MediaFrameSourceGroup, MediaFrameSourceInfo>(result_group, result_info);
+            return result;
         }
     }
 }
